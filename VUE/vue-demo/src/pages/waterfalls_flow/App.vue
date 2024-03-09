ffa4f28246260515f7cf09c07859a80ec3b91022
chunglijung <andy28572463@gmail.com>
2024-03-09 12:57:10 +0800

    init
    1. 初始化 tailwindcss
    2. 初始化 vite + vue mpa

---
+++ VUE/vue-demo/src/pages/waterfalls_flow/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const msg = ref("hello vue");
+</script>
+<template>
+  <div>
+    <h1 class="bg-slate-600 text-3xl font-bold underline">{{ msg }}</h1>
+  </div>
+</template>
+<style scoped></style>
