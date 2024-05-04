87fbfad078901536577ee3ffb6bae157c7124956
chunglijung <andy28572463@gmail.com>
2024-05-04 14:16:18 +0800

    chore: 安裝EF並設定

--- NET/DynamicAuth/Program.cs
+++ NET/DynamicAuth/Program.cs
@@ -1,11 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var config = builder.Configuration;
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuthContext>(options => options.UseSqlServer(config.GetConnectionString("Auth")));
 
 var app = builder.Build();
 
